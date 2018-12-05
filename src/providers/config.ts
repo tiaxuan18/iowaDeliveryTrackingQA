c283382cb78a7e621d61f78a44dc73328a45cdd4
Tia Xuan <tia.xuan@perficient.com>
2018-12-05 11:27:11 -0800

    salesforce sync QA 2

--- src/providers/config.ts
+++ src/providers/config.ts
@@ -1,5 +1,5 @@
 
-export const SERVER_URL = "https://idt-salesforcesync.herokuapp.com";
+export const SERVER_URL = "https://idt-salesforcesyncqa.herokuapp.com";
 export const TRACK_INTERVAL = 60000;
 export const LOGIN_INTERVAL = 24; // in hours
 
